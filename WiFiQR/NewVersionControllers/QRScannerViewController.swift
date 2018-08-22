102a546d05f4dde917c0fdb74b0311646c09d8de
silvicardo <silvicardo@gmail.com>
2018-08-22 19:43:46 +0200

    QRScannerViewController file

---
+++ WiFiQR/NewVersionControllers/QRScannerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  QRScannerViewController.swift
+//  WiFiQR
+//
+//  Created by riccardo silvi on 22/08/2018.
+//  Copyright © 2018 riccardo silvi. All rights reserved.
+//
+
+import UIKit
+
+class QRScannerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
