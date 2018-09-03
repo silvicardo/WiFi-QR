b4d32e728240ff893f11a9ce362d182878c52e1b
silvicardo <silvicardo@gmail.com>
2018-09-03 08:08:29 +0200

    NetworkDetailViewController Sharing Functionality

--- WiFiQR/New Version/NewVersionControllers/NetworkDetailViewController.swift
+++ WiFiQR/New Version/NewVersionControllers/NetworkDetailViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import MessageUI
 
 class NetworkDetailViewController: UIViewController {
-
+    
+    // MARK: - VARIABLES
+    
     var wifiNetwork : WiFiNetwork?
     
     var networkIndex : Int!
     
     let toEditSegueId = "ToEditNetwork"
+    
+    let textForGenericShare : [String] = ["I'm sending you this QR to access network with ssid:  ", ", password: " ]
+    
+    let textForKeepPressedForOptions = "\nKeep the QRCode pressed for two seconds to show import options"
+    
+    let noPassword = "No Password"
+    
+    // MARK: - OUTLETS
     
     @IBOutlet var panToClose: InteractionPanToClose!
     
@@ -35,27 +46,84 @@ class NetworkDetailViewController: UIViewController {
     @IBOutlet weak var networkAndQRrStackView: UIStackView!
     
     @IBOutlet weak var networkStackView: UIStackView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         panToClose.setGestureRecognizer()
         
         CoreDataManagerWithSpotlight.shared.detCont = self
-    
+        
         guard let wiFiNetwork = wifiNetwork else {return}
         
         guard let qrCode = QRManager.shared.generateQRCode(from: wiFiNetwork.wifiQRString!) else {return}
         
         loadUIwith(wiFiNetwork, qr: qrCode)
-            
+        
         
     }
-   
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         panToClose.animateDialogAppear()
+        
+    }
     
+    // MARK: - IB ACTIONS
+    
+    @IBAction func shareNetworkButtonTapped(_ sender: Any) {
+        
+        guard let wifiToShare = wifiNetwork else { return }
+        
+        guard let ssid = wifiToShare.ssid,
+            let password = wifiToShare.password,
+            let qr = QRManager.shared.generateQRCode(from: wifiToShare.wifiQRString!) else { return }
+        
+        let itemsToShare : [Any] = [textForGenericShare[0] + ssid + textForGenericShare[1] + password , qr]
+        
+        let activityVC = UIActivityViewController(activityItems: itemsToShare, applicationActivities: nil)
+        
+        present(activityVC, animated: true, completion: nil)
+        
+        
+    }
+    
+    @IBAction func shareBySmsButtonTapped(_ sender: Any) {
+        
+        guard MFMessageComposeViewController.canSendText() else { return }
+        
+        guard let wifiToShare = wifiNetwork else { return }
+        
+        guard let ssid = wifiToShare.ssid,
+            let password = wifiToShare.password,
+            let qr = QRManager.shared.generateQRCode(from: wifiToShare.wifiQRString!),
+            let qrData = qr.pngData() else { return }
+        
+        let smsController = prepareMFMessageComposeViewControllerWith(ssid: ssid, password: password, qrCode: qrData)
+        
+        present(smsController, animated: true, completion: nil)
+        
+        
+        
+    }
+    
+    @IBAction func shareByEmailButtonTapped(_ sender : Any) {
+        
+        guard MFMailComposeViewController.canSendMail() else { return }
+        
+        guard let wifiToShare = wifiNetwork else { return }
+        
+        guard let ssid = wifiToShare.ssid,
+            let password = wifiToShare.password,
+            let qr = QRManager.shared.generateQRCode(from: wifiToShare.wifiQRString!),
+            let qrData = qr.jpegData(compressionQuality: 1.0) else { return }
+        
+        let mailController = prepareMFMailComposeViewControllerWith(ssid: ssid, password: password, qrCode: qrData)
+        
+        present(mailController, animated: true, completion: nil)
+        
     }
     
     @IBAction func deleteNetworkButtonTapped(_ sender: Any) {
@@ -71,12 +139,12 @@ class NetworkDetailViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    
-    
 }
 
+// MARK: - STATUS BAR
+
 extension NetworkDetailViewController {
-   
+    
     //NASCONDE LA STATUS BAR
     override var prefersStatusBarHidden: Bool {
         return true
@@ -84,6 +152,7 @@ extension NetworkDetailViewController {
 }
 
 
+//MARK: - UI FUNCTIONS
 extension NetworkDetailViewController {
     
     func loadUIwith(_ wifiNetwork : WiFiNetwork,qr qrCode : UIImage ) {
@@ -96,13 +165,13 @@ extension NetworkDetailViewController {
             passwordLabel.text = wifiNetwork.password
         } else {
             
-            passwordLabel.text = "No Password"
-//            passwordDesignabileView.isHidden = true
-//
-//            networkAndQRrStackView.axis = .vertical
-//
-//            networkStackView.alignment = .center
-
+            passwordLabel.text = noPassword
+            //            passwordDesignabileView.isHidden = true
+            //
+            //            networkAndQRrStackView.axis = .vertical
+            //
+            //            networkStackView.alignment = .center
+            
         }
         
         chosenEncryptionLabel.text = wifiNetwork.chosenEncryption
@@ -113,12 +182,14 @@ extension NetworkDetailViewController {
         //regolazione rotazione immagine
         if qrCode.size.width > qrCode.size.height {
             qrCodeImageView.image = UIImage(cgImage: qrCode.cgImage!,
-                                           scale: 1.0,
-                                           orientation: .right)
+                                            scale: 1.0,
+                                            orientation: .right)
         }
-    
+        
     }
 }
+
+// MARK: - NAVIGATION
 
 extension NetworkDetailViewController {
     
@@ -128,10 +199,64 @@ extension NetworkDetailViewController {
             let destination = segue.destination as? NetworkEditViewController,
             let wifi = wifiNetwork{
             
-                destination.wifiNetwork = wifi
+            destination.wifiNetwork = wifi
         }
         
     }
     
     
 }
+
+//MARK: MAIL METHODS
+extension NetworkDetailViewController : MFMailComposeViewControllerDelegate {
+    
+    func prepareMFMailComposeViewControllerWith(ssid: String, password: String, qrCode: Data) -> MFMailComposeViewController {
+        
+        let controller = MFMailComposeViewController()
+        controller.mailComposeDelegate = self
+        controller.addAttachmentData(qrCode, mimeType: "image/png", fileName: "myQrToAdd")
+        controller.setSubject(ssid)
+        controller.setMessageBody(textForGenericShare[0] +  ssid +  textForGenericShare[1] + password + textForKeepPressedForOptions, isHTML: false)
+        controller.setMessageBody( textForGenericShare[0] +  ssid +  textForGenericShare[1] + password + textForKeepPressedForOptions, isHTML: false)
+        
+        return controller
+    }
+    
+}
+
+//MARK: SMS METHODS
+
+extension NetworkDetailViewController : MFMessageComposeViewControllerDelegate {
+    
+    
+    func prepareMFMessageComposeViewControllerWith(ssid: String, password: String, qrCode: Data) -> MFMessageComposeViewController {
+        
+        let controller = MFMessageComposeViewController()
+        controller.messageComposeDelegate = self
+        controller.addAttachmentData(qrCode, typeIdentifier: "image/.png", filename: "image.png")
+        controller.body = textForGenericShare[0] +  ssid +  textForGenericShare[1] + password + textForKeepPressedForOptions
+        
+        return controller
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        
+        //If sending sms is possible proceeds with actions.....
+        controller.dismiss(animated: true, completion: nil)
+        
+        
+        self.dismiss(animated: true, completion: nil)
+        
+        
+        //If sending sms is not possible proceeds with next statement
+        if !MFMessageComposeViewController.canSendText() {
+            
+            controller.dismiss(animated: true, completion: nil)
+            
+        }
+        
+        
+    }
+    
+}
+
